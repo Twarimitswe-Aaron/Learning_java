890e4d1a76a3e9cb7f576fe42516eefb16aa0b01
Twarimitswe-Aaron <aarontwarimitswe@gmail.com>
2025-11-03 15:42:01 +0200

    [+]:FileOutput stream and FileInputStream difference in programming

--- GenericType/EmployeeMain.java
+++ GenericType/EmployeeMain.java
@@ -4,10 +4,11 @@ import java.util.List;
 public class EmployeeMain {
     public static void main(String[] args){
         List<Integer> list1=Arrays.asList(1,2,3,4,5);
-        List<String> list2=Arrays.asList("A","B","C","D","E");
+        List<String> list2=Arrays.asList("Aaron","B","C","D","E");
         displayList(list1);
         displayList(list2);
     }
+    //learn differnece between ? extends and ? super
     public static void displayList(List<?> list){
         for(Object var:list){
             System.out.println(var);
